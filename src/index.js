a78d743c594ded0cc4d6ba270c772d3b1fe53be6
jartek <jartek.91@gmail.com>
2020-04-02 20:58:51 +0530

    a

--- src/index.js
+++ src/index.js
@@ -50,12 +50,15 @@ const textQuestionTemplate = (question) => {
 const buildQuestionBlock = (questionId) => {
   const questionToBeAsked = FOOTBALL_QUESTIONS.filter(question => question.question_id === questionId)
   const questionTemplate = { blocks: [] }
+  console.log('questionToBeAsked' + questionToBeAsked)
 
   if (questionToBeAsked.image_url.length > 0) {
     questionTemplate.blocks.push(imageTemplate(questionToBeAsked))
   }
 
   questionTemplate.blocks.push(textQuestionTemplate(questionToBeAsked))
+
+  console.log(questionTemplate)
 
   return questionTemplate
 }
@@ -144,6 +147,7 @@ app.action('start_frolic', async ({ action, ack, say }) => {
 app.action('theme_selected', async ({ action, ack, say }) => {
   await ack()
   currentQuestionForUser = FOOTBALL_QUESTIONS[0].question_id
+  console.log('currentQuestionForUser' + currentQuestionForUser)
 
   await say(buildQuestionBlock(currentQuestionForUser))
 })
