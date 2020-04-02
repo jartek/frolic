ef7abaf5060d0a2314146e39653c37f471256f2a
jartek <jartek.91@gmail.com>
2020-04-02 20:14:59 +0530

    a

--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
 const { App } = require('@slack/bolt')
 
-// const questions = [
-//   {
-//     text: 'Question text',
-//     image_url: 'https://resources.premierleague.com/photos/2020/03/13/ea2ae5c3-f75b-4681-b9d0-d7ef0126b0e9/Statement_Graphic_PL_FA_EFL_Lilac.png?width=930&height=620'
-//   }
-// ]
-
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 })
-
-// app.command('/frolic', async ({ command, ack, say }) => {
-//   // Acknowledge command request
-//   await ack()
-
-//   try {
-//     await say({
-//       blocks: [{
-//         type: 'image',
-//         image_url: questions[0].image_url,
-//         alt_text: questions[0].text
-//       },
-//       {
-//         type: 'section',
-//         text: {
-//           type: 'mrkdwn',
-//           text: questions[0].text
-//         }
-//       }]
-//     })
-//   } catch (error) {
-//     console.error(error)
-//   }
-// })
-
-// app.message(new RegExp(/.*/), async ({ message, say }) => {
-//   await say('You\'re right!')
-// });
 
 app.command('/frolic', async ({ command, ack, say }) => {
   await ack()
@@ -83,7 +48,7 @@ app.command('/frolic', async ({ command, ack, say }) => {
   })
 })
 
-app.action('/start_frolic', async ({ action, ack, say }) => {
+app.action('start_frolic', async ({ action, ack, say }) => {
   await ack()
 
   await say({
@@ -121,11 +86,11 @@ app.action('/start_frolic', async ({ action, ack, say }) => {
   })
 })
 
-app.action('/start_frolic', async ({ action, ack, say }) => {
+app.action('theme_selected', async ({ action, ack, say }) => {
   await ack()
   await say(action.value)
 })
 
-(async () => {
+;(async () => {
   await app.start(process.env.PORT || 3000)
 })()
