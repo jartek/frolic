394e57c8e88fbb4d09a7b91b1171d078dc586815
jartek <jartek.91@gmail.com>
2020-04-02 23:03:03 +0530

    a

--- src/index.js
+++ src/index.js
@@ -11,10 +11,28 @@ const quizId = 1
 let currentQuestionForUser
 let score = 0
 
+const CRICKET_QUESTIONS =[{
+  quiz_id: quizId,
+  question_id: 101,
+  trigger_question_id: 102,
+  question: 'Who has the most number of ducks in the IPL?',
+  image_url: '',
+  answers: ['harbhajansingh', 'parthivpatel'],
+  answer_explanation: ''
+}, {
+  quiz_id: quizId,
+  question_id: 101,
+  trigger_question_id: 102,
+  question: 'Who has the most number of ducks in the IPL?',
+  image_url: '',
+  answers: ['harbhajansingh', 'parthivpatel'],
+  answer_explanation: ''
+}]
+
 const FOOTBALL_QUESTIONS = [{
   quiz_id: quizId,
   question_id: 1,
-  trigger_question_id: 2,
+  trigger_question_id: null,
   question: 'How many clubs based in London played in the first FA premier League in season 1992-93?',
   image_url: '',
   answers: ['6', 'six'],
@@ -27,6 +45,38 @@ const FOOTBALL_QUESTIONS = [{
   image_url: 'https://tmssl.akamaized.net//images/foto/normal/adam-lallana-fc-liverpool-1582122503-31995.jpg',
   answers: ['england'],
   answer_explanation: 'With 9 goals and 9 Fantasy assists, Lallana became a key player in the 2013-14 Southampton squad. The club played fluent and fun to watch football and showcased a few other young English talents such as Jay Rodriguez, James Ward-Prowse, Nathaniel Clyne and Luke Shaw. In the summer of 2014 Lallana was transferred from Southampton to Liverpool along with teammates Dejan Lovren and Rickie Lambert.'
+}, {
+  quiz_id: quizId,
+  question_id: 3,
+  trigger_question_id: null,
+  question: 'Who scored the first ‘perfect hat-trick’ (left foot, right foot, and header) in the Premier League?',
+  image_url: '',
+  answers: ['jimmyfloydhasselbaink', 'hasselbaink'],
+  answer_explanation: ''
+}, {
+  quiz_id: quizId,
+  question_id: 4,
+  trigger_question_id: null,
+  question: 'Kevin Phillips won the Golden Boot in 1999/2000, who was the next English player to do so?',
+  image_url: '',
+  answers: ['harrykane', 'kane'],
+  answer_explanation: ''
+}, {
+  quiz_id: quizId,
+  question_id: 5,
+  trigger_question_id: null,
+  question: 'Which team embarrassingly recorded the fewest points ever in a Premier League season?',
+  image_url: '',
+  answers: ['derbycounty', 'derby'],
+  answer_explanation: ''
+}, {
+  quiz_id: quizId,
+  question_id: 6,
+  trigger_question_id: null,
+  question: 'Who scored the goal that enabled the Republic of Ireland to beat Italy at the 1994 World Cup finals?',
+  image_url: '',
+  answers: ['rayhoughton', 'houghton'],
+  answer_explanation: ''
 }]
 
 const imageTemplate = (question) => {
@@ -147,6 +197,8 @@ app.action('start_frolic', async ({ action, ack, say }) => {
 
 app.action('theme_selected', async ({ action, ack, say }) => {
   await ack()
+
+  console.log(action.text)
   currentQuestionForUser = findQuestion(FOOTBALL_QUESTIONS[0].question_id)
 
   await say(buildQuestionBlock(currentQuestionForUser))
