e9468a062a9a4d93e21c501e7527f4638492b0d8
jartek <jartek.91@gmail.com>
2020-04-02 19:56:16 +0530

    a

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
+# Frolic
+
 ```mermaid
 sequenceDiagram
-    User ->> Channel: Type /curio start
-    Channel ->> CurioApp: Start quiz for all users in the channel
-    CurioApp ->> CurioApp: Create a quiz room for that org
-    CurioApp ->> User: Ask for a theme?
-    User ->> CurioApp: Select a theme
-    CurioApp ->> User: How many Questions?
-    User ->> CurioApp: 5 questions
-    CurioApp ->> CurioApp: Select questions for quiz based on params
-    CurioApp ->> Slack: Inform All users that quiz is starting
-    Slack ->> CurioBot: Send messages to everyone
-    CurioBot ->> User: Hey do you want to take part in the quiz (Yes/No)
-    User ->> CurioBot: Selects yes
-    CurioBot ->> CurioApp: Add him to a "quiz room" for that channel
-    CurioBot ->> User: Quiz is starting in 5 minutes?
+    User ->> Channel: Type /frolic start
+    Channel ->> FrolicApp: Start quiz for all users in the channel
+    FrolicApp ->> FrolicApp: Create a quiz room for that org
+    FrolicApp ->> User: Ask for a theme?
+    User ->> FrolicApp: Select a theme
+    FrolicApp ->> User: How many Questions?
+    User ->> FrolicApp: 5 questions
+    FrolicApp ->> FrolicApp: Select questions for quiz based on params
+    FrolicApp ->> Slack: Inform All users that quiz is starting
+    Slack ->> FrolicBot: Send messages to everyone
+    FrolicBot ->> User: Hey do you want to take part in the quiz (Yes/No)
+    User ->> FrolicBot: Selects yes
+    FrolicBot ->> FrolicApp: Add him to a "quiz room" for that channel
+    FrolicBot ->> User: Quiz is starting in 5 minutes?
     loop Quiz
-      CurioApp ->> CurioBot: Send Question?
-      CurioBot ->> User: Display the question in the User <> Bot chat
-      User ->> CurioBot: Type Answer
-      CurioBot ->> CurioApp: Record Answer
+      FrolicApp ->> FrolicBot: Send Question?
+      FrolicBot ->> User: Display the question in the User <> Bot chat
+      User ->> FrolicBot: Type Answer
+      FrolicBot ->> FrolicApp: Record Answer
     end
-    CurioApp ->> Slack: Display results in the channel
+    FrolicApp ->> Slack: Display results in the channel
     Slack ->> Channel: Show scores
 ```
--- src/index.js
+++ src/index.js
@@ -1,44 +1,85 @@
 const { App } = require('@slack/bolt')
 
-const questions = [
-  {
-    text: 'Question text',
-    image_url: 'https://resources.premierleague.com/photos/2020/03/13/ea2ae5c3-f75b-4681-b9d0-d7ef0126b0e9/Statement_Graphic_PL_FA_EFL_Lilac.png?width=930&height=620'
-  }
-]
+// const questions = [
+//   {
+//     text: 'Question text',
+//     image_url: 'https://resources.premierleague.com/photos/2020/03/13/ea2ae5c3-f75b-4681-b9d0-d7ef0126b0e9/Statement_Graphic_PL_FA_EFL_Lilac.png?width=930&height=620'
+//   }
+// ]
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 })
 
+// app.command('/frolic', async ({ command, ack, say }) => {
+//   // Acknowledge command request
+//   await ack()
+
+//   try {
+//     await say({
+//       blocks: [{
+//         type: 'image',
+//         image_url: questions[0].image_url,
+//         alt_text: questions[0].text
+//       },
+//       {
+//         type: 'section',
+//         text: {
+//           type: 'mrkdwn',
+//           text: questions[0].text
+//         }
+//       }]
+//     })
+//   } catch (error) {
+//     console.error(error)
+//   }
+// })
+
+// app.message(new RegExp(/.*/), async ({ message, say }) => {
+//   await say('You\'re right!')
+// });
+
 app.command('/frolic', async ({ command, ack, say }) => {
-  // Acknowledge command request
   await ack()
 
-  try {
-    await say({
-      blocks: [{
-        type: 'image',
-        image_url: questions[0].image_url,
-        alt_text: questions[0].text
-      },
+  await say({
+    blocks: [
       {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: questions[0].text
+          text: 'Do you want to frolic?'
         }
-      }]
-    })
-  } catch (error) {
-    console.error(error)
-  }
+      },
+      {
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              emoji: true,
+              text: 'Yes'
+            },
+            style: 'primary',
+            value: 'yes'
+          },
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              emoji: true,
+              text: 'No'
+            },
+            style: 'danger',
+            value: 'no'
+          }
+        ]
+      }
+    ]
+  })
 })
-
-app.message(new RegExp(/.*/), async ({ message, say }) => {
-  await say('You\'re right!')
-});
 
 (async () => {
   await app.start(process.env.PORT || 3000)
